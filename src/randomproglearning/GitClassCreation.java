12dfcec2d7c175d3bbfb905d85f9f8789f463fe9
santosh <sanchoudhary865@gmail.com>
2024-11-23 18:41:07 +0530

    Statging commit

--- src/randomproglearning/GitClassCreation.java
+++ src/randomproglearning/GitClassCreation.java
@@ -6,6 +6,14 @@ public class GitClassCreation {
 		System.out.println("Hello");
 		
 		System.out.println("Hello 2 after push");
+		
+		System.out.println("3rd push");
+	}
+	
+	public static void m1()
+	
+	{
+		System.out.println("Hey !!!! Yo..........");
 	}
 
 }
